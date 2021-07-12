15c184b9c69dc056a551554601634e497560e512
Kingster <kingsterrck@126.com>
2021-07-12 09:10:15 +0800

    July 12 Java

---
+++ LearnAP/July12.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+import java.util.TimeZone;
+
+
+public class July12 {
+    public static void main(String[] arg) {
+        // double a = 25.0/4;
+        // System.out.println(a);
+
+        // System.out.println(Math.pow(2,3.5));
+        
+        // Scanner input = new Scanner(System.in);
+        // double m = input.nextDouble();
+        // double r = input.nextDouble();
+        // r = Math.pow(r,2.0);
+        // System.out.println(m * r);
+
+        //4/(3*(r+34))-9*(a+b*c)+(3+d*(2+a))/(a+b*d);
+        //5.5*Math.pow(r+2.5,2.5+t);
+        //simpleDateFormat.setTimeZone(TimeZone.getTimeZone("GMT+07:00"));
+        //final TimeZone timeZone = TimeZone.getTimeZone("GTM+8");
+        //TimeZone.setDefault(timeZone);
+        // TimeZone china = TimeZone.getTimeZone("GMT+:08:00");
+        // TimeZone chongqing = TimeZone.getTimeZone("Asia/Chongqing");
+        // long totalMilliseconds = System.currentTimeMillis();
+        // long totalSeconds = totalMilliseconds/1000;
+        // long currentSeconds = totalSeconds %60;
+        // long totalMinutes = totalSeconds / 60;
+        // long currentMinutes = totalMinutes % 60;
+        // long totalHours = totalMinutes / 60;
+        // long currentHours = totalHours % 24;
+        
+        // System.out.println(currentHours + ":" + currentMinutes + ":" + currentSeconds);
+
+        // int a = 6;
+        // int b = a++;
+        // System.out.println(a);
+        // System.out.println(b);
+        // a = 6;
+        // b = ++a;
+        // System.out.println(a);
+        // System.out.println(b);
+
+        double a = 2.5;
+        double b = 2.5%1;
+        System.out.println(b);
+        
+    }
+}
