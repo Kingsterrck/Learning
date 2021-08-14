c090d7e0e44a31115e27029f3afd17eb4a48be8f
Kingster <kingsterrck@126.com>
2021-08-14 16:55:53 +0800

    August 14

--- Template/js/getWords.js
+++ Template/js/getWords.js
@@ -1,14 +1,21 @@
 function getWords() {
     var title = document.getElementsByTagName("h1");
     for (i = 0;i<title.length;i++) {
-        var b = title[i].innerText;
+        var b = title[i].innerText; //b is title
     }
-    var langs = document.getElementsByClassName("lang-en TermText");
-    var leng = langs.length;
+    var wordList = document.getElementsByClassName("lang-en TermText"); //get all the words
+    var list = [];
+    for (i=0;i<wordList.length;i++){ // copy words to list
+        list.push(wordList[i]);
+    }
     var output = "@ " + b + "\n";
+    var bfleng = list.length;
     for (i = 1; i < 11; i++) {
-        var a = langs[parseInt(Math.random()*leng)].innerText;
-        output += i + ", " + a + "\n";
+        if (list.length < 10 && i > bfleng){break}
+        var pos = parseInt(Math.random()*list.length);
+        var a = list[pos].innerText;
+        output += i + ". " + a + "\n";
+        list.splice(pos,1);
     }
     console.log(output);
 }
