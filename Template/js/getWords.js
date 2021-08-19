f88507bb7698aa5dc2ea6463493f7ee54fa8c732
Kingster <kingsterrck@126.com>
2021-08-19 21:59:46 +0800

    August 19

--- Template/js/getWords.js
+++ Template/js/getWords.js
@@ -1,4 +1,5 @@
-function getWords() {
+// version 1, random 10 words from the list
+function getWords() { 
     var title = document.getElementsByTagName("h1");
     for (i = 0;i<title.length;i++) {
         var b = title[i].innerText; //b is title
@@ -14,6 +15,41 @@ function getWords() {
         if (list.length < 10 && i > bfleng){break}
         var pos = parseInt(Math.random()*list.length);
         var a = list[pos].innerText;
+        output += i + ". " + a + "\n";
+        list.splice(pos,1);
+    }
+    console.log(output);
+}
+//version 1.1 (or 2), choose the 10 longest words
+function getWords2() { 
+    var title = document.getElementsByTagName("h1");
+    for (i = 0;i<title.length;i++) {
+        var b = title[i].innerText; //b is title
+    }
+    var wordList = document.getElementsByClassName("lang-en TermText"); //get all the words
+    var list = [];
+    for (i=0;i<wordList.length;i++){ // copy words to list
+        list.push(wordList[i].innerText);
+    }
+    var bfleng = list.length;
+    if (bfleng > 10) {
+        for (i = 0;i<list.length;i++) {
+        for (k = 1;k < list.length;k++){
+            if (list[k-1].length < list[k].length) { // if the latter word is longer than the former one
+                list.splice(k-1,0,list[k]);
+                list.splice(k+1,1);
+                }
+            }
+        }
+        for (i=1;i<bfleng-11;i++){
+        list.pop();//get rid of extra words
+    }
+    }
+    var output = "@ " + b + "\n";
+    for (i = 1; i < 11; i++) {
+        if (list.length < 10 && i > bfleng){break}
+        var pos = parseInt(Math.random()*list.length);
+        var a = list[pos];
         output += i + ". " + a + "\n";
         list.splice(pos,1);
     }
